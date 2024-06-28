f14fa5c1b1e6d158c7d809e8f66c9e1dd2bb6e04
Zachary King <zak@delfina.com>
2024-06-28 13:52:38 -0700

    download a file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useMemo, useState } from "react";
 import useSWRImmutable from "swr/immutable";
 
-import { useScript } from "./useScript";
+import { useScript } from "../hooks/useScript";
 
 function useSqlite() {
   const sqliteStatus = useScript("/jswasm/sqlite3.js");
@@ -23,8 +24,37 @@ function useSqlite() {
 
 export default function Home() {
   const { sqlite } = useSqlite();
+  const [db, setDb] = useState<Uint8Array | null>(null);
 
   console.log(sqlite);
 
-  return <main>hello world</main>;
+  async function handleStart() {
+    const downloadUrl = "/GCF_000002765.3.db";
+
+    let bytes = 0;
+
+    const response = await fetch(downloadUrl);
+    const body = response.body;
+    if (!body) {
+      throw Error("Response body is null.");
+    }
+    const reader = body.getReader();
+    reader.read().then(function processText({ done, value }) {
+      if (done) {
+        console.log("Stream complete");
+        return;
+      }
+      if (value) {
+        bytes += value.length;
+        console.log("Received " + bytes + " bytes");
+      }
+      reader.read().then(processText);
+    });
+  }
+
+  return (
+    <main>
+      <button onClick={handleStart}>Start</button>
+    </main>
+  );
 }
